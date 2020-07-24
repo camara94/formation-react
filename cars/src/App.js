46ad1ac433b50726230d4ff7d05f2550d69594ff
camara94 <ldamaro98@gmail.com>
2020-07-24 19:45:11 +0200

    Les class en react

--- cars/src/App.js
+++ cars/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MyCars from './components/MyCars';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editeur hello <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MyCars />
     </div>
   );
 }
