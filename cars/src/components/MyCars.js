5033ef01698f6b8783880a410a1e25befe03dce5
camara94 <ldamaro98@gmail.com>
2020-07-30 12:27:28 +0200

    les evenements en  reactJS

--- cars/src/components/MyCars.js
+++ cars/src/components/MyCars.js
@@ -3,10 +3,23 @@ import Cars from './Cars';
 
 class MyCars extends Component {
 
+   noCopy = () => alert('Merci de ne pas copier ce text !!');
+
+   makeChange = (e) => {
+       if ( e.target.classList.contains('styled') ) {
+           e.target.classList.remove('styled');
+       } else {
+           e.target.classList.add('styled');
+       }
+   }
+
     render() {
         return (
             <div>
-                <h1> {this.props.title} </h1>
+                <h1 onMouseEnter={this.makeChange}> {this.props.title} </h1>
+                <p onCopy={this.noCopy} style={{ width: '400px', margin: '5px auto' }}>Je n'ai pas reçu l'email d'activation
+                Si après 12h de la création de votre compte, vous n'avez toujours pas reçu votre email, cela veut dire que vous vous êtes trompé de votre adresse email, 
+                il faut refaire votre compte de nouveau en faisant attention à saisir votre adresse email.</p>
                 <Cars color='red'> Ford </Cars>
                 <Cars color='green'> Mercedes </Cars>
                 <Cars > Peugeot </Cars>
