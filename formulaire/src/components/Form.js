2a0550f9d43c95b7c5cbba7b3b6291ab827389a8
camara94 <ldamaro98@gmail.com>
2020-07-31 09:11:58 +0200

    Les formulaires en reactJS

--- formulaire/src/components/Form.js
+++ formulaire/src/components/Form.js
@@ -1,13 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Car from './Car';
 
 class Form extends Component {
+    state = {
+        pseudo: null,
+        couleurs: ['', 'red', 'green', 'yellow', 'black'],
+        couleur: null,
+        comment: null
+    };
+
+    inputEvent = (e) => {
+        this.setState(
+            {
+                pseudo: e.target.value
+            }
+        )
+        console.log(e.target.value)
+    };
+
+    handlerColor = e => {
+        this.setState(
+            {
+                couleur: e.target.value
+            }
+        )
+        console.log(e.target.value)
+    };
+
+    handlerComment = e => {
+       this.setState({ comment: e.target.value })
+    }
+
+    sumitHandler = e => {
+        e.preventDefault();
+        console.log(`Pseudo: ${this.state.pseudo} Couleur: ${this.state.couleur} Commentaire: ${this.state.comment}`)
+    }
+
+
     render () {
+        let options = this.state.couleurs.map( (opt, i) => {
+            return (
+                <Fragment key={i} >
+                    <option value={opt} > {opt} </option>
+                </Fragment>
+            )
+        } );
         return (
             <div style={{width: '450px', margin: '5px auto'}}>
                 <Car />
                 <h1>Commentaire</h1>
-                Formulaire
+                <form onSubmit={this.sumitHandler}>
+                    <label for="pseudo">Pseudo: </label>
+                    <input id="pseudo" type="text" value={this.state.pseudo} onChange={this.inputEvent} /> <br />
+                    <label for="couleur">Couleurs: </label>
+                    <select id="couleur" value={this.state.couleur} onChange={this.handlerColor}>
+                        {options}
+                    </select><br/>
+                    <label for="comment">Commentaire: </label>
+                    <textarea id="comment" value={this.state.comment} onChange={this.handlerComment} ></textarea><br /> 
+                    <button type="submit">Valider</button>
+                </form>
             </div>
         );
     }
